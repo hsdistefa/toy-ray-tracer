30a8c3d0d2c6bd4d0fb5ff04bbf9fb9206d4b5cc
hsdistefa <hsdistefa@gmail.com>
2020-12-28 12:37:46 -0800

    Added simple reflections

--- ray_tracer.py
+++ ray_tracer.py
@@ -30,11 +30,15 @@ def nearest_intersected_object(objects, ray_origin, ray_direction):
     return nearest_object, min_distance
 
 
+def reflected(vector, axis):
+    return vector - 2*np.dot(vector, axis) * axis
+
+
 def render():
     # Dimensions of screen output
     # Increase for higher resolution
-    width = 300
-    height = 200
+    height = 1080
+    width = 1920
 
     # Camera position
     camera = np.array([0, 0, 3])
@@ -48,68 +52,86 @@ def render():
     ratio = float(width) / height
     screen = (-1, 1 / ratio, 1, -1 / ratio)  # left, top, right, bottom
 
+    # Maximum number of light reflections to be calculated
+    max_depth = 3
+
     # Object properties
     objects = [
         {'center': np.array([-0.2, 0, -1]), 'radius': 0.7, 'ambient': np.array([0.1, 0, 0]),
-         'diffuse': np.array([0.7, 0, 0]), 'specular': np.array([1, 1, 1]), 'shininess': 100},
+         'diffuse': np.array([0.7, 0, 0]), 'specular': np.array([1, 1, 1]), 'shininess': 100,
+         'reflection': 0.5},
         {'center': np.array([0.1, -0.3, 0]), 'radius': 0.1, 'ambient': np.array([0.1, 0, 0.1]),
-         'diffuse': np.array([0.7, 0, 0.7]), 'specular': np.array([1, 1, 1]), 'shininess': 100},
+         'diffuse': np.array([0.7, 0, 0.7]), 'specular': np.array([1, 1, 1]), 'shininess': 100,
+         'reflection': 0.5},
         {'center': np.array([-0.3, 0, 0]), 'radius': 0.15, 'ambient': np.array([0, 0.1, 0]),
-         'diffuse': np.array([0, 0.6, 0]), 'specular': np.array([1, 1, 1]), 'shininess': 100},
+         'diffuse': np.array([0, 0.6, 0]), 'specular': np.array([1, 1, 1]), 'shininess': 100,
+         'reflection': 0.5},
         {'center': np.array([0, -9000, 0]), 'radius': 9000 - 0.7, 'ambient': np.array([0.1, 0.1, 0.1]),
-         'diffuse': np.array([0.6, 0.6, 0.6]), 'specular': np.array([1, 1, 1]), 'shininess': 100}
+         'diffuse': np.array([0.6, 0.6, 0.6]), 'specular': np.array([1, 1, 1]), 'shininess': 100,
+         'reflection': 0.5}
     ]
 
     image = np.zeros((height, width, 3))
     for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
+        print('progress: %d/%d' % (i + 1, height))
         for j, x in enumerate(np.linspace(screen[0], screen[2], width)):
-            print('progress: %d/%d' % (i + 1, height))
             pixel = np.array([x, y, 0])
             origin = camera
             direction = normalize(pixel - origin)
+            color = np.zeros((3))
+            reflection = 1
 
-            # Check for intersections
-            nearest_object, min_distance = nearest_intersected_object(objects, origin, direction)
-            if nearest_object is None:
-                continue
+            for k in range(max_depth):
+                # Check for intersections
+                nearest_object, min_distance = nearest_intersected_object(objects, origin, direction)
+                if nearest_object is None:
+                    break
 
-            # Compute intersection point between ray and nearest object
-            intersection = origin + min_distance * direction
+                # Compute intersection point between ray and nearest object
+                intersection = origin + min_distance * direction
 
-            # Shift the point perpendicular to the object surface to prevent
-            # intersecting the object itself again
-            normal_to_surface = normalize(intersection - nearest_object['center'])
-            shifted_point = intersection + 1e-5 * normal_to_surface
+                # Shift the point perpendicular to the object surface to prevent
+                # intersecting the object itself again
+                normal_to_surface = normalize(intersection - nearest_object['center'])
+                shifted_point = intersection + 1e-5 * normal_to_surface
 
-            # Direction from object intersection to light source
-            intersection_to_light = normalize(light['position'] - shifted_point)
+                # Direction from object intersection to light source
+                intersection_to_light = normalize(light['position'] - shifted_point)
 
-            _, min_distance = nearest_intersected_object(objects, intersection, intersection_to_light)
-            intersection_to_light_distance = np.linalg.norm(light['position'] - intersection)
-            is_shadowed = min_distance < intersection_to_light_distance
+                _, min_distance = nearest_intersected_object(objects, intersection, intersection_to_light)
+                intersection_to_light_distance = np.linalg.norm(light['position'] - intersection)
+                is_shadowed = min_distance < intersection_to_light_distance
 
-            if is_shadowed:
-                continue
+                if is_shadowed:
+                    break
 
-            # Render object using Blinn-Phong model
+                # Render object using Blinn-Phong model
 
-            # RGB values
-            illumination = np.zeros((3))
+                # RGB values
+                illumination = np.zeros((3))
 
-            # Ambient
-            illumination += nearest_object['ambient'] * light['ambient']
+                # Ambient
+                illumination += nearest_object['ambient'] * light['ambient']
 
-            # Diffuse
-            illumination += nearest_object['diffuse'] * light['diffuse'] * \
-                np.dot(intersection_to_light, normal_to_surface)
+                # Diffuse
+                illumination += nearest_object['diffuse'] * light['diffuse'] * \
+                    np.dot(intersection_to_light, normal_to_surface)
 
-            # Specular
-            intersection_to_camera = normalize(camera - intersection)
-            H = normalize(intersection_to_light + intersection_to_camera)
-            illumination += nearest_object['specular'] * light['specular'] * \
-                np.dot(normal_to_surface, H) ** (nearest_object['shininess'] /4)
+                # Specular
+                intersection_to_camera = normalize(camera - intersection)
+                H = normalize(intersection_to_light + intersection_to_camera)
+                illumination += nearest_object['specular'] * light['specular'] * \
+                    np.dot(normal_to_surface, H) ** (nearest_object['shininess'] /4)
 
-            image[i, j] = np.clip(illumination, 0, 1)
+                # Reflection
+                color += reflection * illumination
+                reflection *= nearest_object['reflection']
+
+                # New ray origin and direction
+                origin = shifted_point
+                direction = reflected(direction, normal_to_surface)
+
+            image[i, j] = np.clip(color, 0, 1)
 
     plt.imsave('image.png', image)
 
